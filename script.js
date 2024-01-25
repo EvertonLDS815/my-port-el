3af811537d12290b4822a391ef7f35172aa42ec1
EvertonLDS815 <evertonlino1234@gmail.com>
2024-01-25 11:45:12 -0300

    feat: create menu-icon

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button = document.querySelector(".button");
-const url = 'https://node-name.vercel.app'
+const menu = document.querySelector(".menu-icon");
+const nav = document.querySelector(".header-port nav");
+const url = 'https://node-name.vercel.app';
 
 add()
 function add() {
@@ -30,3 +32,18 @@ function montarBlog(lista) {
     }
     document.querySelector(".container").innerHTML = html
 }
+function closeMenu() {
+    nav.style.width = "0";
+    // overlay.style.display = "none";
+}
+
+function menuToggle() {
+    if(nav.style.width === "170px") {
+        closeMenu();
+    } else {
+        nav.style.width = "170px";
+        // overlay.style.display = "block";
+    }
+}
+
+menu.addEventListener('click', menuToggle)
